b23b6a3474bd84752690635f54a82766746f2d18
Konrad Kolasa <kolasakonrad1@gmail.com>
2022-12-16 16:17:45 +0100

    basic styles, canvas and Player class setup

---
+++ orbit-shooter/src/classes/Player.class.ts
@@ -0,0 +1,39 @@
+import { c } from '../main';
+
+class Player implements PlayerModel {
+    private _position;
+    private _radius;
+    private _color;
+    constructor({ position, color, radius } : PlayerConstructor) {
+        this._position = position
+        this._radius = radius;
+        this._color = color;
+    }
+
+    get position() {
+        return this._position;
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this._position.x, this._position.y, this._radius, 0, Math.PI * 2, false);
+        c.fillStyle = this._color;
+        c.fill();
+    };
+
+}
+
+interface PlayerModel {
+    position: PositionType,
+    draw: () => void
+}
+
+interface PlayerConstructor {
+    position: PositionType,
+    radius: number,
+    color: string
+}
+
+type PositionType = { x: number, y: number }
+
+export default Player;
--- orbit-shooter/src/main.ts
+++ orbit-shooter/src/main.ts
@@ -1,2 +1,12 @@
 import './assets/styles/style.scss'
+import Player from './classes/Player.class';
+
+export const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+export const c = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const player = new Player({ position: { x: canvas.width / 2, y: canvas.height / 2 }, color: 'blue', radius: 30 })
+player.draw();
 
