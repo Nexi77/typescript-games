4ecb83990f70570f1546ee73348a869e37605642
Konrad Kolasa <kolasakonrad1@gmail.com>
2022-12-19 19:23:29 +0100

    create enemies

--- orbit-shooter/src/main.ts
+++ orbit-shooter/src/main.ts
@@ -1,4 +1,5 @@
 import './assets/styles/style.scss'
+import Enemy from './classes/Enemy.class';
 import Player from './classes/Player.class';
 import Projectile from './classes/Projectile.class';
 
@@ -14,13 +15,44 @@ const canvasCenterVertical = canvas.height / 2;
 const player = new Player({ position: { x: canvasCenterHorizontal, y: canvasCenterVertical }, color: 'blue', radius: 30 });
 
 const projectiles: Projectile[] = [];
+const enemies: Enemy[] = [];
+let hue = 0;
+
+const spawnEnemies = () => {
+    setInterval(() => {
+        const radius = Math.random() * (30 - 5) + 5;
+        const position = {
+            x: 0,
+            y: 0
+        }
+        if(Math.random() < .5) {
+            position.x = Math.random() < .5 ? 0 - radius : canvas.width + radius;
+            position.y = Math.random() * canvas.height;
+        }
+        else {
+            position.x = Math.random() * canvas.width;
+            position.y = Math.random() < .5 ? 0 - radius : canvas.height + radius;
+        }
+        const color = `hsla(${hue},100%,50%,1)`
+        const angle = Math.atan2(
+            canvas.height / 2 -  position.y,
+            canvas.width / 2 - position.x
+        )
+        const velocity = { 
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+        enemies.push(new Enemy({ position, radius, color, velocity }))
+    }, 1000)
+};
 
 const animate = () => {
     requestAnimationFrame(animate);
     c.clearRect(0,0, canvas.width, canvas.height);
     player.draw();
+    hue++;
     projectiles.forEach(projectile => projectile.update());
-    
+    enemies.forEach(enemy => enemy.update());
 };
 
 addEventListener('click', (e) => {
@@ -28,16 +60,15 @@ addEventListener('click', (e) => {
         e.clientY - canvas.height / 2,
         e.clientX - canvas.width / 2
     )
-
     const velocity = { 
         x: Math.cos(angle),
         y: Math.sin(angle)
     }
-
     projectiles.push(
         new Projectile({ position: { x: canvasCenterHorizontal, y: canvasCenterVertical}, radius: 5, color: 'red', velocity})
     )
 });
 
 animate();
+// spawnEnemies();
 
